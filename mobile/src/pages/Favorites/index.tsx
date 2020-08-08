b408d2ee4f764e5a2dd07b8183928a780943c478
João Pedro <joao.goulart@internit.com.br>
2020-08-07 18:10:31 -0700

    Finalização do projeto

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { View, ScrollView, AsyncStorage } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+
+import styles from './styles';
+import PageHeader from '../../Components/PageHeader';
+import TeacherItem, { Teacher } from '../../Components/TeacherItem';
+
+function TeacherList() {
+    const [favorites, setFavorites] = useState([]);
+    
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if(response) {
+                const favoritedTeachers = JSON.parse(response);
+
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
+    return (
+        <View style={styles.container}>
+            <PageHeader  title="Meus Proffys favoritos"/>
+
+            <ScrollView
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16
+                }}
+            >
+
+                {favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem
+                            key={teacher.id} 
+                            teacher={teacher}
+                            favorited
+                        />
+                    )
+                })}
+            </ScrollView>
+        </View>
+    )
+}
+
+export default TeacherList;
